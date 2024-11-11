1fa77394a1a3f7dc50da0cfebbc56b0e0d7ec7dd
lishasharma172 <shalinisolanki.2009@gmail.com>
2024-11-11 13:34:48 +0530

    commit1

---
+++ src/test/java/pakage1/ContactTest.java
@@ -0,0 +1,14 @@
+package pakage1;
+
+public class ContactTest {
+
+	public static void main(String[] args) {
+		
+			System.out.println("execute createContactTest");
+		
+
+		
+			System.out.println("execute modifyContactTest");
+		
+	}
+}
---
+++ src/test/java/pakage1/OrgTest.java
@@ -0,0 +1,11 @@
+package pakage1;
+
+public class OrgTest {
+
+	public static void main(String[] args) {
+		System.out.println("execute createORgTest");
+
+		System.out.println("execute modifyOrgTest");
+	}
+
+}
